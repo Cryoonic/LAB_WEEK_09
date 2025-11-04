745aa37024515bb332299f31f1fd66a78186209e
Cryoonic <albertalexander2004@gmail.com>
2025-11-04 23:59:39 +0700

    Commit No. 2 – State and Event Handler

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -23,19 +26,24 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import androidx.compose.foundation.text.KeyboardOptions
 
 class MainActivity : ComponentActivity() {
+
+    data class Student(
+        var name: String
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LAB_WEEK_09Theme {
-                Surface (
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ){
-                    val list = listOf("Tanu", "Tina", "Tono")
-                    Home(list)
+                ) {
+                    Home()
                 }
             }
         }
@@ -43,48 +51,79 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Home(
-    items: List<String>
-){
+fun Home() {
+    val listData = remember {
+        mutableStateListOf(
+            MainActivity.Student("Tanu"),
+            MainActivity.Student("Tina"),
+            MainActivity.Student("Tono")
+        )
+    }
+
+    val inputField = remember { mutableStateOf(MainActivity.Student("")) }
+
+    HomeContent(
+        listData = listData,
+        inputField = inputField.value,
+        onInputValueChange = { input ->
+            inputField.value = inputField.value.copy(name = input)
+        },
+        onButtonClick = {
+            if (inputField.value.name.isNotBlank()) {
+                listData.add(MainActivity.Student(inputField.value.name))
+                inputField.value = MainActivity.Student("")
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeContent(
+    listData: SnapshotStateList<MainActivity.Student>,
+    inputField: MainActivity.Student,
+    onInputValueChange: (String) -> Unit,
+    onButtonClick: () -> Unit
+) {
     LazyColumn {
         item {
             Column(
-                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text(text = stringResource(
-                    id = R.string.enter_item)
-                )
+            ) {
+                Text(text = stringResource(id = R.string.enter_item))
 
                 TextField(
-                    value = "",
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number
-                    ),
-                    onValueChange = {
-
-                    }
+                    value = inputField.name,
+                    onValueChange = { onInputValueChange(it) },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    modifier = Modifier.padding(top = 8.dp)
                 )
-                Button(onClick = { }) {
-                    Text(text = stringResource(
-                        id = R.string.button_click)
-                    )
+
+
+                Button(
+                    onClick = { onButtonClick() },
+                    modifier = Modifier.padding(top = 12.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.button_click))
                 }
             }
         }
-        items(items){ item ->
-            Column (
-                modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text(text = item)
-            }
+        
+        items(listData) { item ->
+            Text(
+                text = item.name,
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewHome(){
-    Home(listOf("Tanu", "Tina", "Tono"))
+fun PreviewHome() {
+    LAB_WEEK_09Theme {
+        Home()
+    }
 }
